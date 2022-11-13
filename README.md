9b8cc0952427c255e72b7abf04c7182b52027672
Mephistoteles28 <ladomahardika1@gmail.com>
2022-11-13 22:48:06 +0700

    Add notes and add .sql files

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 So this is my project on Secure Programming.
 This web called Silk Road, the idea is to make a drugs store online.
 
+## How to setup
+
+1. download this directory by 'git clone https://github.com/Mephistoteles28/secprog-silkroad.git'
+2. Import the `silkroad.sql' to your database, such as phpmyadmin
+3. Copy this file to the server, in xampp copy this file to `xampp/htdocs/
+4. Go to the web app with type 'localhost/secprog-silkroad'
+
 ## Credential
 
 For user,
---
+++ silkroad.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2022 at 04:42 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `silkroad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `product_price` varchar(50) NOT NULL,
+  `product_img` varchar(100) NOT NULL,
+  `product_quantity` int(100) NOT NULL,
+  `product_description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_img`, `product_quantity`, `product_description`) VALUES
+(1, 'Aspirin', '13000', 'aspirin.jpg', 20, 'Aspirin is a salicylate (sa-LIS-il-ate). It works by reducing substances in the body that cause pain, fever, and inflammation.\nAspirin is used to treat pain, and reduce fever or inflammation. '),
+(2, 'Adderall', '45000', 'adderall.jpg', 50, 'Adderall is used to treat attention deficit hyperactivity disorder (ADHD) and narcolepsy. Adderall contains a combination of amphetamine and dextroamphetamine.'),
+(3, 'Benztropine', '39000', 'benztropine-mesylate.jpg', 45, 'Benztropine is used with other medicines to treat symptoms of Parkinson\'s disease, such as stiffness or tremors.'),
+(4, 'Benzonatate', '32000', 'benzonatate.jpg', 30, 'Benzonatate is a non-narcotic cough medicine.\r\nBenzonatate works by numbing the throat and lungs, making the cough reflex less active.'),
+(5, 'Cephalexin', '45000', 'cephalexin.jpg', 32, 'Cephalexin is a cephalosporin (SEF a low spor in) antibiotic. It works by fighting bacteria in your body.\nCephalexin is used to treat infections caused by bacteria, including upper respiratory infect'),
+(9, 'aaaaa', '123', '636f9e97242ea2017-02-08 (1).png', 132, '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'user', '$2y$10$yoChafH38xZTIclBY/ic1.9e7E7kkQyzoz6jMPhWt9Ffa6Dd4AWtC');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

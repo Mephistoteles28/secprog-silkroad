85ce62702f112be5c1aae84358e3db877d403985
Lovella_ <81017144+Lovellaa@users.noreply.github.com>
2022-12-07 09:16:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-require 'inc/function.php';
 session_start();
 // Cek apakah belum login, kalau belum maka akan dilempar ke login page
-if (!isset($_SESSION["login"])){
-    header("Location: login.php");
+if (!isset($_SESSION["login-admin"])){
+    header("Location: login-admin.php");
 }
+
+require 'function.php';
+
 $products = query("SELECT * FROM products");
 
-if(!$products){
-    echo "<br>";
-    echo mysqli_error($conn);
-    echo "<br>";
+// Find command
+if(isset($_POST["find"])){
+    $products = cari($_POST);
 }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -23,47 +22,60 @@ if(!$products){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
-    <title>Home</title>
+    <link rel="stylesheet" href="../style.css?v=<?php echo time(); ?>">
+    <title>Admin Page</title>
 </head>
 
 <body>
     <!-- Navbar -->
     <nav>
         <div class="left-nav">
-            <img src="img/SilkRoad-logo.png" alt="logo" width="100px">
-            <a href="index.php" id="navbar-item"> Home </a>
-            <a href="contact-us.php" id="navbar-item"> Contact Us </a>
+            <img src="../img/SilkRoad-logo.png" alt="logo" width="100px">
+            <a href="../index.php" id="navbar-item"> User Page </a>
         </div>
         <div class="right nav">
-            <a href="about-me"><img src="img/user-icon.png" alt="user-icon" width="50px"></a>
+            <p id="navbar-item"> Admin Panel </p>
+            <form action="" method="post">
+                <input type="text" name="keyword" autofocus autocomplete="off" placeholder="Type here">
+                <button type="submit" name="find">Find!</button>
+            </form>
         </div>
     </nav>
 
+
     <!-- Content -->
-    <div class="content">
+    <h2><a href="tambah.php"> Add products </a></h2>
+    <br>
+    <table border=2px cellpadding=10px cellspacing=0px;>
+        <tr>
+            <th>No</th>
+            <th>Action</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Desription</th>
+            <th>Image</th>
+        </tr>
+        <?php $i = 1 ?>
         <?php foreach($products as $product): ?>
-        <div class="content-item">
-            <img src="img/<?=$product["product_img"]?>" alt="<?=$product["product_img"]?>" id="item-image">
-            <div>
-                <h3><?=$product["product_name"]?></h3>
-            </div>
-            <div>
-                <p><b>Rp <?=$product["product_price"]?></b></p>
-                <p><?=$product["product_description"]?></p>
-            </div>
-            <div id="item-detail">
-                <a href="https://go.drugbank.com/unearth/q?searcher=drugs&query=<?=$product["product_name"]?>" alt="<?=$product["product_img"]?>">more details</a>
-            </div>
-        </div>
+        <tr>
+            <td><?= $i ?></td>
+            <td><a href="edit.php?id=<?=$product["id"]?>">edit</a>
+                | <a href="hapus.php?id=<?= $product["id"] ?>" onclick="return confirm('Yakin?')">
+                    hapus</a></td>
+            <td><?= $product["product_name"] ?></td>
+            <td><?= $product["product_price"] ?></td>
+            <td><?= $product["product_quantity"] ?></td>
+            <td><?= $product["product_description"] ?></td>
+            <td><img src="../img/img_drug/<?= $product["product_img"] ?>" alt="<?= $product["product_img"] ?>"
+                    style="width:100px">
+            </td>
+        </tr>
+        <?php $i++; ?>
         <?php endforeach ?>
-    </div>
-    <a href="logout.php"> Logout </a>
+    </table>
 
-    <!-- Footer -->
-    <div class="footer">
-
-    </div>
+    <a href="logout.php"> Logout as Admin </a>
 
 </body>
 
